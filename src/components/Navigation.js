4fbc82c86ca21179fadde40d9fd8053458c4df78
Dmytro Arkhypov <dimonarh@gmail.com>
2020-06-25 16:50:55 +0700

    React portfolio structure Ready Nav menu

---
+++ src/components/Navigation.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+export default function Navigation () {
+    const navLinks = [
+        {
+            title: 'Home',
+            path: '/'
+        },
+        {
+            title: 'Portfolio',
+            path: '/portfolio'
+        },
+        {
+            title: 'Contact Us',
+            path: '/contact-us'
+        }
+    ]
+
+    const socialLinks = [
+        {
+            title: 'Be',
+            path: '/'
+        },
+        {
+            title: 'P',
+            path: '/'
+        },
+        {
+            title: 'Li',
+            path: '/'
+        }
+    ]
+    return (
+        <nav className="site-navigation">
+            <span>Angy Nicko</span>
+            <ul>
+                { socialLinks.map((link, index) => (
+                    <li key={index}>
+                        {link.title}
+                    </li>
+                ))
+
+                }
+            </ul>
+            <ul>
+            { navLinks.map((link, index) => (
+                <li key={index}>
+                <Link to={link.path}>{link.title}</Link>
+                </li>
+            ))}
+            
+            </ul>
+        </nav>
+    )
+}
